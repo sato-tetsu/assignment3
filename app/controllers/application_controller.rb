642508c2bb0303a366ea74243bba693cc0282ce6
vagrant <vagrant@localhost.localdomain>
2020-06-03 03:37:13 +0000

    [Update]5-9

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,13 @@
 class ApplicationController < ActionController::Base
 	before_action :configure_permitted_parameters, if: :devise_controller?
-	before_action :authenticate_user!
+
+	def after_sign_in_path_for(resource)  # ログイン後に遷移するpathを設定
+		user_path(current_user.id)		# マイページに遷移するよう指示
+	end
+
+	def after_sign_out_path_for(resource)  # ログアウト後に遷移するpathを設定
+		top_path	# topに戻るよう指示
+	end
 
   protected
   def configure_permitted_parameters
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,15 @@ Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   #get '/' => 'books#top'
-  root 'books#index'
-  resources :books, only: [:new, :create, :index, :show, :destroy]
-  resources :users, only: [:show, :edit, :update]
+
+  root to: "home#top"
+
+  get "users/:id" => "users#show", as: :mypage
+  get "home/top" => "home#top", as: :top
+  get "home/about" => "home#about"
+
+  resources :books, only: [:new, :create, :index, :show, :edit, :update, :destroy]
+  resources :users, only: [:index, :show, :edit, :update]
 end
+
+
