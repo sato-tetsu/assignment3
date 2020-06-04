4607794c01e2a0aad02b7906c7ec798f25833cd4
vagrant <vagrant@localhost.localdomain>
2020-06-04 06:41:27 +0000

    [Finish]5-9

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -14,14 +14,24 @@ class UsersController < ApplicationController
   end
 
   def edit
-  	@user = User.find(params[:id])
+      @user = User.find(params[:id])
+
+      if @user.id != current_user.id
+        redirect_to user_path(current_user.id)
+      end
   end
 
   def update
   	@user = User.find(params[:id])
-  	@user.update(user_params)
-  	redirect_to user_path(@user.id)
+  	if @user.update(user_params)
+      flash[:notice] = "successfully"
+      redirect_to user_path(@user.id)
+    else
+      flash[:alert2] = @user.errors.full_messages
+      render :edit
+    end
   end
+
 
   private 
 	def user_params
